b59e5bc5904dedeb91c64d2c9b4b832387bb0a8b
DesiZafirova <desszafirowa@gmail.com>
2024-08-19 21:57:47 +0300

    components and pages commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import Homepage from './pages/Homepage';
+import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
+import AppLayout from './pages/AppLayout';
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
